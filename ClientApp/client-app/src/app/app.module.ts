daea9ed01467d81ef2ed54b34b3d804f82a2edba
earg <earg@citlali.mx>
2022-09-26 18:03:29 -0600

    x

--- ClientApp/client-app/src/app/app.module.ts
+++ ClientApp/client-app/src/app/app.module.ts
@@ -6,14 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { SideMenuComponent } from './pages/side-menu/side-menu.component';
+import { SideMenuComponent } from './shared/side-menu/side-menu.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    SideMenuComponent
+    SideMenuComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
